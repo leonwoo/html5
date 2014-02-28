26e0c458e5c673e150d0a8cf46415e77c97bf1b7
leon.wu <leon.wooo@gmail.com>
2014-03-01 00:40:37 +1100

    Handle puzzle without solution.

--- sudokuSolver/sudokuSolver/js/default.js
+++ sudokuSolver/sudokuSolver/js/default.js
@@ -1,28 +1,10 @@
 ﻿(function () {
     //"use strict";
-    function getStyleRuleValue(style, selector, sheet) {
-        var sheets = typeof sheet !== 'undefined' ? [sheet] : document.styleSheets;
-        for (var i = 0, l = sheets.length; i < l; i++) {
-            var sheet = sheets[i];
-            if (!sheet.cssRules) { continue; }
-            for (var j = 0, k = sheet.cssRules.length; j < k; j++) {
-                var rule = sheet.cssRules[j];
-                if (rule.selectorText && rule.selectorText.split(',').indexOf(selector) !== -1) {
-                    return rule.style[style];
-                }
-            }
-        }
-        return null;
-    }
 
+    // Get all cells
     var cells = document.querySelectorAll("#grid td");
     for (var i = 0; i < cells.length; i++) {
         cells[i].addEventListener("click", onCellClicked, false);
-    }
-
-    function onCellClicked(event) {
-        var cell = event.target;
-        cell.innerText = selectedNum;
     }
 
     var numBtns = document.querySelectorAll(".Numbertable td");
@@ -34,6 +16,14 @@
 
     var unselectNumBtnColor = getStyleRuleValue("borderColor", ".numBtn");
     var selectedNum = "";
+    var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    var grids = [];
+    var route = [];
+    var NO_SOLUTION = 1;
+    var solveButton = document.querySelector("#solve");
+    solveButton.addEventListener("click", solveClickedHandler, false);
+
     function numBtnClicked(event) {
         for (var i = 0; i < numBtns.length; i++) {
             numBtns[i].style.borderColor = unselectNumBtnColor;
@@ -42,21 +32,20 @@
         selectedNum = event.target.innerText;
     }
 
-    var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    var grids = [];
-    var route = [];
-
-    var solveButton = document.querySelector("#solve");
-    solveButton.addEventListener("click", solveClickedHandler, false);
+    function onCellClicked(event) {
+        var cell = event.target;
+        cell.innerText = selectedNum;
+    }
 
     function solveClickedHandler() {
+        // Build the grid
         for (var i = 0; i < cells.length; i++) {
             if (i % 9 === 0) {
                 grids.push([]);
             }
             var cell = cells[i].innerText;
             var value = parseInt(cell);
+
             var y = Math.floor(i / 9);
 
             if (!isNaN(value) && value >= 0 && value <= 9) {
@@ -71,88 +60,98 @@
         playGame();
     }
     
+    // Game loop
     function playGame()
     {
-        // Try to figure out as much as possible cells in the 1st round
-        for (; ;) {
-            var candidatesAll = getCandidatesAll();
-            var blankCellNum = candidatesAll.blankCellNum;
-            var candidatesArr = candidatesAll.candidatesArr;
-            // No blank cell, it's the solution already
-            if (blankCellNum == 0) {
-                showGrid();
-                return;
-            }
-
-            // Keep feeling the cells with a solid answer
-            var count = 0;
-            for (var i = 0; i < candidatesArr.length; i++) {
-                var candidate = candidatesArr[i];
-                if (candidate.c.length == 1) {
-                    count++;
-                    grids[candidate.y][candidate.x] = candidate.c[0];
-                }
-            }
-            if (count == 0)
-            {
-                // no more solid candidate, jump out
-                break;
-            }
-        }
-
-        for (; ;)
-        {
-            var candidatesAll = getCandidatesAll();
-            var blankCellNum = candidatesAll.blankCellNum;
-            var candidatesArr = candidatesAll.candidatesArr;
-            // No blank cell, we have a result now
-            if (blankCellNum == 0) {
-                showGrid();
-                return;
-            }
-
-            var deadEnd = _.find(candidatesArr, function (n) { return n.c.length == 0; }) !== undefined;
-            if (deadEnd) {
-                // Back to the begin, there is no solution
-                if (route.length == 0) {
-                    console.log("no solution");
+        try {
+            // Try to figure out as much as possible cells in the 1st round
+            for (; ;) {
+                var candidatesAll = getCandidatesAll();
+                var blankCellNum = candidatesAll.blankCellNum;
+                var candidatesArr = candidatesAll.candidatesArr;
+                // No blank cell, it's the solution already
+                if (blankCellNum == 0) {
+                    showGrid();
                     return;
                 }
-                else {
-                    // This route can not lead to the result, back 1 step
 
-                    // First purge point without candidates
-                    var numPtToRemove = 0;
-                    for (var i = route.length - 1; i >= 0; i--) {
-                        var pt = route[i];
-                        if (pt.c.length == 0) {
-                            grids[pt.y][pt.x] = 0;
-                            numPtToRemove++;
-                        }
-                        else {
-                            break;
-                        }
+                // Keep feeling the cells with a solid answer
+                var count = 0;
+                for (var i = 0; i < candidatesArr.length; i++) {
+                    var candidate = candidatesArr[i];
+                    if (candidate.c.length == 1) {
+                        count++;
+                        grids[candidate.y][candidate.x] = candidate.c[0];
                     }
+                }
+                if (count == 0) {
+                    // no more solid candidate, jump out
+                    break;
+                }
+            }
 
-                    route = route.slice(0, route.length - numPtToRemove);
+            for (; ;) {
+                var candidatesAll = getCandidatesAll();
+                var blankCellNum = candidatesAll.blankCellNum;
+                var candidatesArr = candidatesAll.candidatesArr;
+                // No blank cell, we have a result now
+                if (blankCellNum == 0) {
+                    showGrid();
+                    return;
+                }
+
+                var deadEnd = _.find(candidatesArr, function (n) { return n.c.length == 0; }) !== undefined;
+                if (deadEnd) {
+                    // Back to the begin, there is no solution
                     if (route.length == 0) {
                         console.log("no solution");
                         return;
                     }
                     else {
-                        var lastPt = route[route.length - 1];
-                        grids[lastPt.y][lastPt.x] = lastPt.c.pop();
-                    }
-                    continue;
-                }
-            }
+                        // This route can not lead to the result, back 1 step
 
-            var candidate = candidatesArr[0];
-            grids[candidate.y][candidate.x] = candidate.c.pop();
-            route.push(candidate);
+                        // First purge point without candidates
+                        var numPtToRemove = 0;
+                        for (var i = route.length - 1; i >= 0; i--) {
+                            var pt = route[i];
+                            if (pt.c.length == 0) {
+                                grids[pt.y][pt.x] = 0;
+                                numPtToRemove++;
+                            }
+                            else {
+                                break;
+                            }
+                        }
+
+                        route = route.slice(0, route.length - numPtToRemove);
+                        if (route.length == 0) {
+                            console.log("no solution");
+                            return;
+                        }
+                        else {
+                            var lastPt = route[route.length - 1];
+                            grids[lastPt.y][lastPt.x] = lastPt.c.pop();
+                        }
+                        continue;
+                    }
+                }
+
+                var candidate = candidatesArr[0];
+                grids[candidate.y][candidate.x] = candidate.c.pop();
+                route.push(candidate);
+            }
+        }
+        catch (e) {
+            if (e === NO_SOLUTION) {
+                alert("No solution");
+            }
+            else {
+                alert("Something wrong, but I don't know the reason.")
+            }
         }
     }
 
+    // Get candiates for all empty cells and check the existing cells to detect unsolvable puzzle
     function getCandidatesAll() {
         var candidatesArr = [];
         var blankCellNum = 0;
@@ -162,11 +161,17 @@
                     blankCellNum++;
                     candidatesArr.push({ x: x, y: y, c: getCandiates(x, y) });
                 }
+                else {
+                    if (getCandiates(x, y).indexOf(grids[y][x]) == -1) {
+                        throw NO_SOLUTION;
+                    }
+                }
             }
         }
         candidatesArr = _.sortBy(candidatesArr, function (n) { return n.c.length; });
         return { candidatesArr: candidatesArr, blankCellNum: blankCellNum };
     }
+
     function showGrid() {
         for (var i = 0; i < cells.length; i++) {
             var cell = cells[i];
@@ -179,6 +184,7 @@
         }
     }
 
+    // Find out all candiates for a cell
     function getCandiates(x, y) {
         var row = [];
         for (var i = 0; i < 9; i++) {
@@ -212,5 +218,19 @@
         return _.difference(result, box3X3);
     }
 
+    function getStyleRuleValue(style, selector, sheet) {
+        var sheets = typeof sheet !== 'undefined' ? [sheet] : document.styleSheets;
+        for (var i = 0, l = sheets.length; i < l; i++) {
+            var sheet = sheets[i];
+            if (!sheet.cssRules) { continue; }
+            for (var j = 0, k = sheet.cssRules.length; j < k; j++) {
+                var rule = sheet.cssRules[j];
+                if (rule.selectorText && rule.selectorText.split(',').indexOf(selector) !== -1) {
+                    return rule.style[style];
+                }
+            }
+        }
+        return null;
+    }
 
 })();
