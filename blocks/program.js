f572a6b4c075604e8953e01005b4f147b246138f
leon.wu <leon.wooo@gmail.com>
2014-03-13 14:27:01 +1100

    more udpates.

--- blocks/program.js
+++ blocks/program.js
@@ -267,6 +267,7 @@
                     }
                     if (!generateBlock()) {
                         endGame();
+                        return;
                     }
                 }
             }
